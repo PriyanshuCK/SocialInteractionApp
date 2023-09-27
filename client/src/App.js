e733bdf09d4cca28507f83a91bb7bdf0aaf82951
PriyanshuCK <priyanshu.sharma.rampura@gmail.com>
2023-09-27 20:41:24 +0530

    Added variable themes

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,27 @@ import { Outlet, Navigate, Route, Routes, useLocation } from "react-router-dom";
 import { Home, Login, Profile, Register, ResetPassword } from "./pages";
 import { useSelector } from "react-redux";
 
+const colors = [
+  "red",
+  "orange",
+  "amber",
+  "yellow",
+  "lime",
+  "green",
+  "emerald",
+  "teal",
+  "cyan",
+  "sky",
+  "blue",
+  "indigo",
+  "violet",
+  "purple",
+  "fuchsia",
+  "pink",
+  "rose",
+  "slate",
+];
+
 function Layout() {
   const { user } = useSelector((state) => state.user);
   const location = useLocation();
@@ -13,9 +34,11 @@ function Layout() {
 }
 
 function App() {
+  const value = Math.floor(Math.random() * 16);
   const { theme } = useSelector((state) => state.theme);
+  const color = colors[value];
   return (
-    <div data-theme={theme} className={`w-full min-h-[100vh] theme-rose ${theme}`}>
+    <div data-theme={theme} className={`w-full min-h-[100vh] theme-${color} ${theme}`}>
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
